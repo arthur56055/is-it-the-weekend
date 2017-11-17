f92928fe03808c3c24a85b0613304d0b15890480
Arturo Ocampo <arturoo56055@students.laalliance.org>
2017-11-17 20:27:32 +0000

    I'm done with my lab.

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var answer = 'NO'
+
+    
+function setup(){
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw(){
+    background('dodgerblue')
+    textSize(32)
+    fill('white')
+    text('Is it the weekend ' + answer, 40, 40)
+}
